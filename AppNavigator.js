1461bb8fb1fb6d12b32c934bd1bd8e1cacb725de
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-27 23:28:10 +0700

    🍏 badge for tab navigation

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/AntDesign';
+import { Badge } from 'react-native-elements'
 
 import Category from './screens/Category';
 import Categories from './screens/Categories';
 import Cart from './screens/Cart';
 import Orders from './screens/Orders';
 import Settings from './screens/Settings';
+import { AppContext } from './contexts/AppContext';
 
 const color = {
   ACTIVE: 'blue',
@@ -32,11 +35,29 @@ const CartStack = createStackNavigator({ Cart });
 CartStack.navigationOptions = {
   tabBarLabel: 'Cart',
   tabBarIcon: ({ focused }) => {
-    return <Icon 
-      name='shoppingcart'
-      size={36}
-      color={focused ? color.ACTIVE : color.INACTIVE}
-    />
+    return <View>
+      <Icon 
+        name='shoppingcart'
+        size={36}
+        color={focused ? color.ACTIVE : color.INACTIVE}
+      />
+      <AppContext.Consumer>
+        {
+          ({totalPrice}) => {
+            if (totalPrice > 0) {
+              return (
+                <Badge
+                  status="error"
+                  value='1+'
+                  containerStyle={{ position: 'absolute', top: -4, right: -4 }}
+                />
+              );
+            } 
+            return null;
+          }
+        }
+      </AppContext.Consumer>
+    </View>
   }
 };
 
